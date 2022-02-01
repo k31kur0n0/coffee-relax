64509174af48d43eb4b091164fa2efa822eae7ad
Mayol Jose <mayol.jose.f@gmail.com>
2022-01-31 21:24:30 -0300

    - Adds a navbar - Adds example of component call whit routing - Adds a image for the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CombosComponent } from './components/combos/combos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'combos', component : CombosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CombosComponent } from './components/combos/combos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CombosComponent
   ],
   imports: [
     BrowserModule,
