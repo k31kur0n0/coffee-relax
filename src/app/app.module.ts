a96eabd86408a628f2dea2e241cd7bb7b19dfcc9
Mayol Jose <mayol.jose.f@gmail.com>
2022-02-04 00:52:19 -0300

    Add container component, this component will be containg all other components to be show in the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
